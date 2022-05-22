f0ce784fa25ecf6ac8ad7b0cc4dd07cbffca8275
Jayanth <jayanthkumar597@gmail.com>
2022-05-22 22:06:37 +0530

    Django update

---
+++ pricenotify.py
@@ -0,0 +1,7 @@
+from urllib import response
+import requests
+bitcoin_api_url= 'https://api.coinmarketcap.com/v1/ticker/bitcoin'
+response = requests.get (bitcoin_api_url)
+response_json = response.json
+type(response_json)
+##print (response_json)
