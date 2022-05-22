9b76fc668799093cca13521d390b93ed9020b248
Jayanth <jayanthkumar597@gmail.com>
2022-05-22 22:28:48 +0530

    Updates

--- pricenotify.py
+++ pricenotify.py
@@ -1,7 +1,11 @@
+bitcoin_api_url= 'https://api.coinmarketcap.com/v1/ticker/bitcoin'
 from urllib import response
 import requests
-bitcoin_api_url= 'https://api.coinmarketcap.com/v1/ticker/bitcoin'
-response = requests.get (bitcoin_api_url)
-response_json = response.json
-type(response_json)
-##print (response_json)
+
+def get_latest_bitcoin_price():
+    response = requests.get (bitcoin_api_url)
+    response_json = response.json()
+    return float (response_json[0]['price_usd'])
+    def main():
+            price = get_latest_bitcoin_price()
+            print ("Bitcoin Price is:"+ price)
