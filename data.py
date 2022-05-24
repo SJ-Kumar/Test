948069dfa2c77a69310b8504c8280529cf9c9860
Jayanth <jayanthkumar597@gmail.com>
2022-05-24 20:28:49 +0530

    SQL file-WIP

---
+++ data.py
@@ -0,0 +1,15 @@
+## This file is stil throwing error....need to debug how to connect to localhost...should an instance of server should run in parallel or SQL server instance should run?---Need to explore..
+
+import mysql.connector as SQLC
+# Establishing connection with the SQL
+ 
+DataBase = SQLC.connect(
+  host ="localhost",
+  user ="user name",
+  password ="password"
+)
+# Cursor to the database
+Cursor = DataBase.cursor()
+ 
+Cursor.execute("CREATE DATABASE margam-data")
+print("Margam Farms database is created")
