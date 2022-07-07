0065bb59d68b47df2b35d0a2b174dfd56b94a382
Jayanth <jayanthkumar597@gmail.com>
2022-07-07 09:27:16 +0530

    Update

---
+++ datamachine.py
@@ -0,0 +1,22 @@
+
+import os
+print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
+print("PATH:", os.environ.get('PATH'))
+
+
+import secrets
+import random
+
+#To read the dataset
+import numpy as ny
+import pandas as pd 
+
+#For visualization
+#import matplotlib.pyplot as plt
+
+print ("First step")
+#Read the dataset
+#uber_df= pd.read_csv("uber-raw-data-sep14.csv")
+
+#Display the first 5 records
+#uber_df.head(5)
